04af6a5c761e1c03ab22bcb69f81f156da37107f
Reem <reemfawazalotaibi11@gmail.com>
2026-04-16 01:25:13 +0300

    lab done

--- Blogger/Blogger/posts/urls.py
+++ Blogger/Blogger/posts/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('add/', views.add_post, name='add_post'),
+    path('<int:id>/', views.detail_post, name='detail_post'),
+    path('<int:id>/update/', views.update_post, name='update_post'),
+    path('<int:id>/delete/', views.delete_post, name='delete_post'),
 ]
--- Blogger/Blogger/posts/views.py
+++ Blogger/Blogger/posts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Post
 
 def add_post(request):
@@ -16,3 +16,38 @@ def add_post(request):
         return redirect('home') 
 
     return render(request, 'posts/add_post.html')
+
+
+def detail_post(request, id):
+    post = get_object_or_404(Post, id=id)
+
+    first_post = Post.objects.all().order_by('-published_at').first()
+
+    return render(request, 'posts/post_detail.html', {
+        'post': post,
+        'first_post': first_post
+    })
+
+
+def update_post(request, id):
+    post = get_object_or_404(Post, id=id)
+
+    if request.method == "POST":
+        post.title = request.POST.get('title')
+        post.content = request.POST.get('content')
+        post.is_published = request.POST.get('is_published') == 'on'
+        post.save()
+        return redirect('detail_post', id=post.id)
+
+    return render(request, 'posts/update_post.html', {'post': post})
+
+
+
+def delete_post(request, id):
+    post = get_object_or_404(Post, id=id)
+
+    if request.method == "POST":
+        post.delete()
+        return redirect('home')
+
+    return render(request, 'posts/delete_post.html', {'post': post})
